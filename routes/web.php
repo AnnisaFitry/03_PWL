61cf96c9b118c7e8a5d3a7da45e027ac80a87f0c
Raditya Catur Narendra <2041720198@student.polinema.ac.id>
2022-03-12 21:11:37 +0700

    test
    p

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\PageCont;
 // Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+//p
 Route::get('/', function () {
     return view('index');
 });
